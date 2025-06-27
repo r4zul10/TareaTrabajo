6028d661afce6ce8497d19086d48a5194ee32f4b
r4zul10 <45101171+r4zul10@users.noreply.github.com>
2025-06-27 17:56:00 -0400

    Update UserService.java

--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -1,7 +1,11 @@
 package com.example.demo.service;
 
+import java.util.Date;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.UserCreateResponseDTO;
 import com.example.demo.entity.User;
 import com.example.demo.error.EmailDuplicadoException;
 import com.example.demo.repository.UserRepository;
@@ -21,6 +25,24 @@ public class UserService {
 		}
 
 		return userRepository.save(user);
+	}
+	
+	
+	
+	public UserCreateResponseDTO createUser(User user) {
+	    User userCreated = save(user); // O `userRepository.save(user)`, dependiendo de tu diseño
+
+	    UUID uuid = UUID.randomUUID();
+	    UserCreateResponseDTO userDTO = new UserCreateResponseDTO();
+
+	    userDTO.setId(userCreated.getId());
+	    userDTO.setCreated(new Date().toString());
+	    userDTO.setModified("No se puede modificar un usuario al menos si no se le está enviando el ID");
+	    userDTO.setLastLogin(new Date().toString());
+	    userDTO.setToken(uuid.toString());
+	    userDTO.setIsActive(true);
+
+	    return userDTO;
 	}
 
 }
