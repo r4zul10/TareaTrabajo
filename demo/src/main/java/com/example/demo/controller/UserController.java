b1340e705b381ba512221c6fe2e345161638f884
r4zul10 <45101171+r4zul10@users.noreply.github.com>
2025-06-27 17:55:30 -0400

    Update UserController.java

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,8 +1,5 @@
 package com.example.demo.controller;
 
-import java.util.Date;
-import java.util.UUID;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,16 +21,8 @@ public class UserController {
 
 	@PostMapping
 	public ResponseEntity<UserCreateResponseDTO> createUser(@RequestBody User user) {
-		User userCreated = service.save(user);
-		UserCreateResponseDTO userDTO = new UserCreateResponseDTO();
-		UUID uuid = UUID.randomUUID();
-		userDTO.setId(userCreated.getId());
-		userDTO.setCreated(new Date().toString());
-		userDTO.setModified("No se puede modificar un usuario al menos si no se le está enviando el ID");
-		userDTO.setLastLogin(new Date().toString());
-		userDTO.setToken(uuid.toString());
-		userDTO.setIsActive(true);
-		return ResponseEntity.ok(userDTO);
+	    UserCreateResponseDTO userDTO = service.createUser(user);
+	    return ResponseEntity.ok(userDTO);
 	}
 
 }
