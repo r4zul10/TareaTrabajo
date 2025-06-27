afcd1c9a1574db1c3f720a88fec321e033ebac0c
r4zul10 <45101171+r4zul10@users.noreply.github.com>
2025-06-27 12:44:46 -0400

    Update UserService.java

--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -1,11 +1,7 @@
 package com.example.demo.service;
 
-import java.util.Date;
-import java.util.UUID;
-
 import org.springframework.stereotype.Service;
 
-import com.example.demo.dto.UserCreateResponseDTO;
 import com.example.demo.entity.User;
 import com.example.demo.error.EmailDuplicadoException;
 import com.example.demo.repository.UserRepository;
@@ -18,24 +14,13 @@ public class UserService {
 		this.userRepository = userRepository;
 	}
 
-	   public UserCreateResponseDTO createUser(User user) {
-	        // Validar email duplicado, si aplica
-	        if (userRepository.existsByEmail(user.getEmail())) {
-	            throw new EmailDuplicadoException("El correo ya registrado");
-	        }
+	public User save(User user) {
 
-	        User userCreated = userRepository.save(user);
+		if (userRepository.existsByEmail(user.getEmail())) {
+			throw new EmailDuplicadoException("El correo ya registrado");
+		}
 
-	        UUID uuid = UUID.randomUUID();
+		return userRepository.save(user);
+	}
 
-	        UserCreateResponseDTO userDTO = new UserCreateResponseDTO();
-	        userDTO.setId(userCreated.getId());
-	        userDTO.setCreated(new Date().toString());
-	        userDTO.setModified("No se puede modificar un usuario al menos si no se le está enviando el ID");
-	        userDTO.setLastLogin(new Date().toString());
-	        userDTO.setToken(uuid.toString());
-	        userDTO.setIsActive(true);
-
-	        return userDTO;
-	    }
 }
